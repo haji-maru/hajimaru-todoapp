a9e14987d6407ce1c1dd709dc7665b2ccc7c7555
hajimaru <hajime4515@gmail.com>
2025-02-13 06:23:07 +0900

    profile 更新 追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,9 +6,31 @@ before_action :authenticate_user!
   end
 
   def edit
-    @profile = current_user.build_profile
+    if current_user.profile.present?
+      @profile = current_user.profile
+    else
+      @profile = current_user.build_profile
+    end
   end
 
   def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィールを更新'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
   end
 end
