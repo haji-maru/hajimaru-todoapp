796b8e27352b61e8e53019750f38592e929cf358
hajimaru <hajime4515@gmail.com>
2025-02-16 11:09:15 +0900

    board task 表示の紐づけ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,25 @@
 class TasksController < ApplicationController
+  before_action :set_board, only: [:index, :show, :new, :create]
   before_action :set_task, only: [:edit, :update]
   before_action :authenticate_user!
 
   def index
-    @tasks = Task.all
+    @tasks = @board.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = @board.tasks.find(params[:id])
   end
 
   def new
-    @task = current_user.tasks.build
+    @task = @board.tasks.build
   end
 
   def create
-    @task = current_user.tasks.build(task_params)
+    @task = @board.tasks.build(task_params)
+    @task.user = current_user
     if @task.save
-      redirect_to tasks_path, notice: '保存できました'
+      redirect_to board_tasks_path, notice: '保存できました'
     else
       flash.now[:error] = '保存に失敗しました'
       render :new
@@ -29,7 +31,7 @@ class TasksController < ApplicationController
 
   def update
     if @task.update(task_params)
-      redirect_to task_path, notice: '更新できました'
+      redirect_to board_tasks_path, notice: '更新できました'
     else
       flash.now[:error] = '更新に失敗しました'
       render :edit
@@ -39,12 +41,16 @@ class TasksController < ApplicationController
   def destroy
     task = current_user.tasks.find(params[:id])
     task.destroy!
-    redirect_to task_path, notice: '削除できました'
+    redirect_to board_tasks_path, notice: '削除できました'
   end
 
   private
   def task_params
     params.require(:task).permit(:title, :content, :due_date, :eyecatch)
+  end
+
+  def set_board
+    @board = Board.find(params[:board_id])
   end
 
   def set_task
