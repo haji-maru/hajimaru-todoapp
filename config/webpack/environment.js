16a6b60b48121d539c2a3d18322f4108e66cb334
hajimaru <hajime4515@gmail.com>
2025-03-02 11:31:35 +0900

    デバック 強制的にnode options 指定(非推奨)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,6 @@ const { environment } = require("@rails/webpacker");
 module.exports = environment;
 
 // コンソールに出力
-console.log("***********************************************");
+console.log("******** config/webpack/environment.js ********");
 console.log("NODE_OPTIONS:", process.env.NODE_OPTIONS);
 console.log("***********************************************");
